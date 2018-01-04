1dc06d342fdad3127fdd73d65d438a0b63c7299d
Mr. Bross <32577977+zbross@users.noreply.github.com>
2018-01-04 18:21:43 -0500

    Initial commit

---
+++ src/robot/Robot.java
@@ -0,0 +1,33 @@
+package robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+    @Override
+    public void robotInit() { }
+
+    @Override
+    public void disabledInit() { }
+
+    @Override
+    public void autonomousInit() { }
+
+    @Override
+    public void teleopInit() { }
+
+    @Override
+    public void testInit() { }
+
+
+    @Override
+    public void disabledPeriodic() { }
+    
+    @Override
+    public void autonomousPeriodic() { }
+
+    @Override
+    public void teleopPeriodic() { }
+
+    @Override
+    public void testPeriodic() { }
+}
