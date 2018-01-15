c15e92089a118fe1efeef7d6fd444228c5553c14
gregoryhazen <gregoryhazen@gmail.com>
2018-01-15 10:45:02 -0500

    got rid of unneeded

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -61,18 +61,6 @@ public class Robot extends IterativeRobot {
 
         SmartDashboard.putData("Autonomous Modes", autoChooser);
 
-        Waypoint[] points = new Waypoint[] {
-                new Waypoint(-4, -1, Pathfinder.d2r(-45)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
-                new Waypoint(-2, -2, 0),                        // Waypoint @ x=-2, y=-2, exit angle=0 radians
-                new Waypoint(0, 0, 0)                           // Waypoint @ x=0, y=0,   exit angle=0 radians
-        }
-
-        Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0);
-        Trajectory trajectory = Pathfinder.generate(points, config);
-
-        TankModifier modifier = new TankModifier(trajectory).modify(0.5);
-        File myFile = new File("/home/lvuser/myfile.traj");
-        Pathfinder.writeToFile(myFile, trajectory);
     }
 
     @Override
