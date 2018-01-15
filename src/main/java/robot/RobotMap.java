28c2b632b70420515f9daab4deb4bc18fe8e8d22
Simar Kareer <skareer@outlook.com>
2018-01-15 09:22:04 -0500

    FIxing Tele-op code

---
+++ src/main/java/robot/RobotMap.java
@@ -0,0 +1,16 @@
+package robot;
+
+public class RobotMap {
+    //Motors
+    public static final int
+            PORT_MOTOR_DRIVE_LEFT_MAIN 			= 1, //Front Left
+            PORT_MOTOR_DRIVE_LEFT_2 			= 2, //Back left
+            PORT_MOTOR_DRIVE_LEFT_3 			= 3,//Top left
+            PORT_MOTOR_DRIVE_RIGHT_MAIN 		= 11,//Front right
+            PORT_MOTOR_DRIVE_RIGHT_2 			= 12,//Back right
+            PORT_MOTOR_DRIVE_RIGHT_3 			= 13;//Top right
+
+    //Controllers
+    public static final int
+            PORT_XBOX_DRIVE = 0;
+}
